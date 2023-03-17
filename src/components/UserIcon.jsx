87a1d9ef97caaf13894fc78e52543210aefeb4fe
rnjswltn1020 <rnjswltn1020@naver.com>
2023-03-17 10:46:00 +0900

    Feat: master - 로그인 유저 이미지 error 뜨는것 수정.
    1. 로그인 유저 이미지 error 뜨는것 수정.

--- src/components/UserIcon.jsx
+++ src/components/UserIcon.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export default function UserIcon({ user }) {
     return (
         <ProfileWrapper>
-            <ProfileImg src={user.photoURL} />
+            <img src={user.photoURL} alt={user.displayName} />
             <UserName>{!user.isAdmin ? user.displayName : '관리자🎅'}</UserName>
         </ProfileWrapper>
     );
@@ -14,18 +14,18 @@ const ProfileWrapper = styled.div`
     width: 55px;
     margin-right: 10px;
 
+    & > img {
+        height: 60%;
+        border-radius: 50px;
+        position: absolute;
+        left: -8px;
+        top: 50%;
+        transform: translateY(-50%);
+    }
+
     @media only screen and (max-width: 768px) {
         width: auto;
     }
-`;
-
-const ProfileImg = styled.img`
-    height: 60%;
-    border-radius: 50px;
-    position: absolute;
-    left: -8px;
-    top: 50%;
-    transform: translateY(-50%);
 `;
 
 const UserName = styled.span`
