f681b355d8ce0807717eafd950c225b7fe43e3db
rnjswltn1020 <rnjswltn1020@naver.com>
2023-03-25 00:24:42 +0900

    Feat: master - 페이지네이션 생성
    1. 페이지네이션 컴포넌트 생성
    2. 상품 페이지에 적용중..

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,20 @@
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
+
+export default function PaginationBox({ data, rowsPerPage, onChangePage }) {
+    const perPage = rowsPerPage || 9;
+    const calcPageCount = () => {
+        const totalLen = data.length;
+        console.log(totalLen);
+        if (totalLen < perPage) {
+            return 1;
+        } else {
+            return Math.ceil(totalLen / perPage);
+        }
+    };
+    return (
+        <Stack spacing={2}>
+            <Pagination count={calcPageCount()} size="large" onChange={onChangePage} />
+        </Stack>
+    );
+}
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,33 +1,62 @@
-import { useQuery } from '@tanstack/react-query';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import { getProductsList } from '../api/fbase';
 import ProductCard from '../components/ProductCard';
+import useProducts from '../hooks/useProduct';
+import PaginationBox from '../components/Pagination';
 
 export default function Products() {
     const {
-        isLoading,
-        error,
-        data: products,
-    } = useQuery({
-        queryKey: ['products'],
-        queryFn: () => getProductsList(),
-    });
+        getProducts: { isLoading, error, data: products },
+    } = useProducts();
+
+    const [getCurrentPage, setCurrentPage] = useState(1);
+    const [getItemPerPage, setItemPerPage] = useState(9);
+    const [targetItems, setTargetItems] = useState(null);
+
+    const indexOfLast = getCurrentPage * getItemPerPage;
+    const indexOfFirst = indexOfLast - getItemPerPage;
+    const currentPosts = posts => {
+        if (posts !== undefined) {
+            let currentPost = 0;
+            currentPost = posts.slice(indexOfFirst, indexOfLast);
+            setTargetItems(currentPost);
+        }
+    };
+
+    useEffect(() => {
+        currentPosts(products);
+    }, [products, getCurrentPage]);
+
+    const handlePage = (e, currentPage) => setCurrentPage(currentPage);
+
     if (isLoading) return 'Loading....';
     if (error) return '에러가 발생하였습니다.';
 
     return (
         <Wrapper>
-            {products.map(item => {
-                return <ProductCard key={item.id} data={item} />;
-            })}
+            <ul>
+                {targetItems &&
+                    targetItems.map(item => {
+                        return <ProductCard key={item.id} data={item} />;
+                    })}
+            </ul>
+            {products && <PaginationBox data={products} onChangePage={handlePage} />}
         </Wrapper>
     );
 }
 
-const Wrapper = styled.ul`
+const Wrapper = styled.section`
     display: flex;
-    flex-wrap: wrap;
-    justify-content: space-around;
-    overflow: hidden;
-    gap: 10px;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    & > ul {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: flex-start;
+        overflow: hidden;
+        gap: 10px;
+        margin-bottom: 20px;
+    }
 `;
